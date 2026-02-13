8a8de4c46053616899db749fd3e919a6d1ee00a0
Dapo Oloyede <dapooloyede@Dapos-MacBook-Pro.local>
2026-02-13 16:47:44 -0700

    autocreate entities

--- src/Data/AppDbContext.cs
+++ src/Data/AppDbContext.cs
@@ -8,4 +8,13 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
     public DbSet<Item> Items => Set<Item>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>().HasData(
+            new Item { Id = 1, Name = "Widget", Description = "A standard widget", CreatedAt = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc) },
+            new Item { Id = 2, Name = "Gadget", Description = "A fancy gadget", CreatedAt = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc) },
+            new Item { Id = 3, Name = "Doohickey", Description = "A useful doohickey", CreatedAt = new DateTime(2025, 1, 1, 0, 0, 0, DateTimeKind.Utc) }
+        );
+    }
 }
--- src/Program.cs
+++ src/Program.cs
@@ -24,7 +24,7 @@ app.MapHealthChecks("/healthz");
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 
 app.Run();
