83151fe89095e3b62d83127fa0dc2545cf49ccd7
Dapo Oloyede <dapooloyede@Dapos-MacBook-Pro.local>
2026-02-15 15:36:23 -0700

    fix seed data error

---
+++ src/Migrations/20260215223543_UpdateSeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetpgmicroservice.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "A standard widget in db");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Items",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Description",
+                value: "A standard widget");
+        }
+    }
+}
